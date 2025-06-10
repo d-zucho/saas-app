d25871af5511493906a45b24ed37f5ae000c8c6f
d-zucho <danny.nico.dev@outlook.com>
2025-06-10 04:20:48 -0700

    add companions and recent sessions

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,52 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Hello title</h1>
+    <main>
+      <h1 className='text-2xl underline'>Popular Companions</h1>
+
+      <section className='home-section'>
+        <CompanionCard
+          id='123'
+          name='Neura the Brainy Explorer'
+          topic='Neural Network of the Brain'
+          subject='Science'
+          duration={45}
+          color='#E5D0FF'
+        />
+        <CompanionCard
+          id='456'
+          name='Countsy the Number Wizard'
+          topic='Derivatives and Integrals'
+          subject='Maths'
+          duration={30}
+          color='#ffda6e'
+        />
+        <CompanionCard
+          id='789'
+          name='Verba the Vocabulary Builder'
+          topic='English Literature'
+          subject='Language'
+          duration={30}
+          color='#BDE7FF'
+        />
+      </section>
+
+      <section className='home-section'>
+        <CompanionsList
+          title='Recently Completed Sessions'
+          companions={recentSessions}
+          classNames='w-2/3 max-lg:w-full'
+        />
+        <CTA />
+      </section>
       <Button>Get started</Button>
-    </div>
+    </main>
   )
 }
 

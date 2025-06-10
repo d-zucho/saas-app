27b873074821948137c7da34b9ca70d67fe187a7
d-zucho <danny.nico.dev@outlook.com>
2025-06-10 03:00:45 -0700

    initial

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className='text-2xl underline'>Hello title</h1>
+      <Button>Get started</Button>
+    </div>
+  )
+}
+
+export default Page

e042432b68cf350ac41d1c89e4c36130932df2ba
d-zucho <danny.nico.dev@outlook.com>
2025-06-14 03:09:09 -0700

    add search input and filter

--- lib/actions/companions.actions.ts
+++ lib/actions/companions.actions.ts
@@ -3,8 +3,9 @@
 import { auth } from '@clerk/nextjs/server'
 import { createSupabaseClient } from '@/lib/supabase'
 
-// CAN ONLY RUN ON SERVER
-// because it accesses database
+// CAN ONLY RUN ON SERVER since we are accessing the auth object and supabase client
+
+// create new companion
 export const createCompanion = async (formData: CreateCompanion) => {
   const { userId: author } = await auth()
   const supabase = createSupabaseClient()
@@ -20,4 +21,37 @@ export const createCompanion = async (formData: CreateCompanion) => {
     )
 
   return data[0]
+}
+
+// fetch all companions
+export const getAllCompanions = async ({
+  limit = 10,
+  page = 1,
+  subject,
+  topic,
+}: GetAllCompanions) => {
+  const supabase = createSupabaseClient()
+
+  let query = supabase
+    .from('Companions')
+    .select() // get all companions
+    .order('created_at', { ascending: false }) // order by created_at in descending order
+
+  if (subject && topic) {
+    query = query
+      .ilike('subject', `%${subject}%`) // filter by subject
+      .or(`topic.ilike.%${topic}%, name.ilike.%${topic}%`) // filter by topic or name
+  } else if (subject) {
+    query = query.ilike('subject', `%${subject}%`)
+  } else if (topic) {
+    query = query.or(`topic.ilike.%${topic}%, name.ilike.%${topic}%`)
+  }
+  // paginate results
+  query = query.range((page - 1) * limit, page * limit - 1)
+
+  const { data: companions, error } = await query
+
+  if (error) throw new Error(error.message || 'Failed to fetch companions')
+
+  return companions || []
 }
